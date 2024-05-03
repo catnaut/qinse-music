6cb90ae5a5c8724481d9a947736d2c1285820e33
catnaut <catnaut@outlook.com>
2024-05-04 00:56:19 +0800

    :bug: fix: 修复了移动设备表单过小

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,13 +3,13 @@ import { LoginForm } from "@/components/form/login";
 export default function Page() {
   return (
     <main className="h-screen w-screen flex justify-center items-center">
-      <div className="hidden  w-1/2 bg-muted h-full lg:block">
+      <div className="hidden w-1/2  h-full bg-muted lg:block" >
         {/* 块底部描述 */}
         <div className="absolute bottom-8 left-8">
           <p className="text-lg text-muted-foreground">块底部描述</p>
         </div>
       </div>
-      <div className="flex-grow w-fu h-full flex justify-center items-center">
+      <div className="lg:w-1/2 w-full flex justify-center items-center">
         <div className="p-8 w-full max-w-md  flex flex-col justify-center items-center ">
           <h1 className="text-3xl font-bold mb-4">Login</h1>
           <LoginForm />
