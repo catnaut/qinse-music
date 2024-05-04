4c05cf1a2e77cdffc9aa924ca21414531053a1f8
catnaut <catnaut@outlook.com>
2024-05-04 22:25:12 +0800

    :memo: docs: 修改网站元信息

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -8,8 +8,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'QinSe Music',
+  tagline: '畅享全平台音乐',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -66,7 +66,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'QinSe Music',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
@@ -76,7 +76,7 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Document',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
