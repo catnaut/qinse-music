2828af19b4ad7d1ddb14b126f3340e29b268f19f
catnaut <catnaut@outlook.com>
2024-05-10 21:23:04 +0800

    :lipstick: style: format code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className=" flex min-h-screen flex-col justify-center items-center">
+    <main className=" flex min-h-screen flex-col items-center justify-center">
       <h1 className="text-4xl">Hello world</h1>
     </main>
   );
