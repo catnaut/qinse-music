d0b0175b6951972bae68617adea668bbe7547e8e
irooke <ARookieA@outlook.com>
2024-05-12 01:12:22 +0800

    🔨 chore: 调整了page.tsx部分没有用tailwindcss的css代码，在imagewithbutton组件去掉了style，换成了classname

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Link from 'next/link';//不写这个就原地tp了
+import Link from 'next/link';//(现在用不上这个了)
 import ImageWithButton from '../components/homepage/imagewithbutton';
 
 const HomePage: React.FC = () => {
@@ -8,22 +8,22 @@ const HomePage: React.FC = () => {
       <main className="flex flex-col justify-start items-start">
         <div className="featured-songs text-xl">
           <div className="song mb-4">
-            <div className="flex items-center">
+            <div className="flex items-start ">
               <div className="flex flex-col ml-4">
                 <h1 className="text-3xl text-orange-600">今日热门</h1>
                 <p className="text-xl text-green-600">推荐的歌曲</p>
               </div>
-                <ImageWithButton image="/images/song1.jpg" alt="Song 1" link="/songs/123124" size={200} style={{ width: '200px' }} />
+                <ImageWithButton image="/images/song1.jpg" alt="Song 1" link="/songs/123124" size={200} className="w-48 h-auto" />
             </div>
           </div>
           <br></br>
           <div className="song mb-4">
-            <div className="flex items-center">
+            <div className="flex items-start ">
               <div className="flex flex-col ml-4">
                 <h1 className="text-3xl text-orange-600">今日热门</h1>
                 <p className="text-xl text-green-600">推荐的歌曲</p>
               </div>
-                   <ImageWithButton image="/images/song2.jpg" alt="Song 2" link="/songs" size={200} style={{ width: '200px' }} />
+                   <ImageWithButton image="/images/song2.jpg" alt="Song 2" link="/songs" size={200} className="w-48 h-auto" />
             </div>
           </div>
           {/* 添加更多歌曲 */}
--- src/components/homepage/imagewithbutton.tsx
+++ src/components/homepage/imagewithbutton.tsx
@@ -6,12 +6,12 @@ interface ImageWithButtonProps {
   alt: string;
   link: string;//添加链接属性
   size: number;
-  style?: React.CSSProperties; // 添加样式属性
+  className?: string; // 添加 className 属性
 }
 
-const ImageWithButton: React.FC<ImageWithButtonProps> = ({ image, alt, link, size, style }) => {
+const ImageWithButton: React.FC<ImageWithButtonProps> = ({ image, alt, link, size,className }) => {
   return (
-    <div className="image-with-button">
+    <div className={`image-with-button ${className}`}>
       {/* 将图像包裹在链接中 */}
       <Link href={link}>
           <img src={image} alt={alt} className="image" style={{ width: size, height: 'auto' }} />
