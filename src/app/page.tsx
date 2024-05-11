d36d8c439b03030a9b2d278a2e98a3e433183971
irooke <ARookieA@outlook.com>
2024-05-11 16:22:06 +0800

    🔨 chore: 解决了原地跳转问题，后面可以尝试用url搜索（本次还没设计）

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,27 +8,23 @@ const HomePage: React.FC = () => {
       <main className="flex flex-col justify-start items-start">
         <div className="featured-songs text-xl">
           <div className="song mb-4">
-          <Link href={"/songs/"}>
             <div className="flex items-center">
               <div className="flex flex-col ml-4">
                 <h1 className="text-3xl text-orange-600">今日热门</h1>
                 <p className="text-xl text-green-600">推荐的歌曲</p>
               </div>
-              <ImageWithButton image="/images/song1.jpg" alt="Song 1" link="/" size={200} style={{ width: '200px' }} />
+                <ImageWithButton image="/images/song1.jpg" alt="Song 1" link="/songs/123124" size={200} style={{ width: '200px' }} />
             </div>
-            </Link>
           </div>
           <br></br>
           <div className="song mb-4">
-          <Link href={"/songs"}>
             <div className="flex items-center">
               <div className="flex flex-col ml-4">
                 <h1 className="text-3xl text-orange-600">今日热门</h1>
                 <p className="text-xl text-green-600">推荐的歌曲</p>
               </div>
-              <ImageWithButton image="/images/song2.jpg" alt="Song 2" link="/" size={200} style={{ width: '200px' }} />
+                   <ImageWithButton image="/images/song2.jpg" alt="Song 2" link="/songs" size={200} style={{ width: '200px' }} />
             </div>
-            </Link>
           </div>
           {/* 添加更多歌曲 */}
         </div>
